e336cd991dff492d1317bdb4a52fefe3713fb940
tcurtis526 <34872688+tcurtis526@users.noreply.github.com>
2023-12-20 13:17:00 -0500

    This was all done last night, but I didn't commit. ahhhhh

--- app/page.tsx
+++ app/page.tsx
@@ -1,110 +1,101 @@
+"use client"
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
+import Fuse from 'fuse.js';
 
-export default function Home() {
+//We got a card Type
+type Card = {
+  ID?:string;
+  Category?:string;
+  Description?:string;
+  Image?:string;
+  Price?:number;
+  Title?:string;
+}
+//And it's props
+type CardProps = {
+  card:Card;
+}
+
+//Function to display a "Card"
+function Card({card}:CardProps){
+  const [hover, setHover] = useState(false);
+  const {ID, Category, Description, Image, Price, Title}=card;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          welcome
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/smallcapsule.png"
-              alt="Vercel Logo"
-              width={200}
-              height={124}
-              priority
-            />
-          </a>
-        </div>
+    <div onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} 
+    style={{borderRadius:5, border:"1px solid white", padding:20, margin:10, maxWidth:500, backgroundColor:(hover ? 'gray':'initial')}}>
+      <div style={{fontSize:24, fontWeight:'bold'}}>
+      {Title}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] "
-          src="/382e1d0b31077ba47d5d770674a81afa.png"
-          alt="Next.js Logo"
-          width={300}
-          height={300}
-          priority
-        />
+      <br/>
+      {Category}
+      <br/>
+      <div style={{fontSize:12,opacity:.6}}>
+      {Description}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://blankgamesonline.com/HopDodge/hopdodge.html"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Demo{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Play the version that started it all!
-          </p>
-        </a>
-
-        <a
-          href="http://hopdodge.com"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            WEBSITE{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about hopdodge!
-          </p>
-        </a>
-
-        <a
-          href="https://www.youtube.com/channel/UCGZOacEkUDNsUrr2EYeoHHQ"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            YOUTUBE{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Pretty cool stuff.
-          </p>
-        </a>
-
-        <a
-          href="https://twitter.com/HopDodge/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            TWITTER{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            yabba dabba dooo
-          </p>
-        </a>
-      </div>
-    </main>
+      <br/>
+      <img src={Image}></img>
+      <div style={{fontSize:24, fontWeight:'bold'}}>${Price}</div>
+      <br/>
+    </div>
   )
 }
+
+export default function Home() {
+  const [searchPattern, setSearchPattern] = useState("");
+  const [data, setData] = useState<Card[]>([]); 
+  const [results, setResults] = useState(data);
+  const fuse = new Fuse(data, fuseOptions);
+  useEffect(() => {
+    const headers = { 'x-api-key': '085e119d-92ef-435e-8ba8-ae7b2e946d65' }
+    // GET request using fetch inside useEffect React hook
+    fetch('https://technical-interview-api.arthrex.io/products', {headers})
+        .then(response => response.json())
+        .then(data => setData(data));
+  }, []);
+
+  useEffect(() => {
+    //using Fuse we can do fuzzy searches on the dataset.
+    if(searchPattern != ""){
+      const resultsBeforeMap = fuse.search(searchPattern)
+      setResults(resultsBeforeMap.map(r => r.item));
+    }
+    //if the search patter is "" just setResults to data
+    else{
+      setResults(data);
+    }
+  }, [data, searchPattern]);
+  return(
+    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flex: 1 }}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+      <div style={{fontSize:42, justifyContent:'center'}}>
+          Welcome to the tool shop!
+      </div>
+      <input style={{color:'black', padding:5}} placeholder='Search' type='text' value={searchPattern} onChange={(e)=>setSearchPattern(e.target.value)}></input>
+        {results.map(card => <Card card={card} />)}
+      </div>
+    </div>
+  )
+}
+
+
+const fuseOptions = {
+  // isCaseSensitive: false,
+  // includeScore: false,
+  // shouldSort: true,
+  // includeMatches: false,
+  // findAllMatches: false,
+  // minMatchCharLength: 1,
+  // location: 0,
+  // threshold: 0.6,
+  // distance: 100,
+  // useExtendedSearch: false,
+  // ignoreLocation: false,
+  // ignoreFieldNorm: false,
+  // fieldNormWeight: 1,
+  keys: [
+    "Title",
+    "Description",
+    "Category"
+  ]
+};
